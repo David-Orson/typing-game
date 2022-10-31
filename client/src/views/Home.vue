1b4bda67dca5d45e67aeff53467ef59d90b00f95
OrsonDC <onlyorson@outlook.com>
2022-10-31 15:31:06 +0100

    initial commit, tailwind and primevue

---
+++ client/src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container mx-auto">
+    <div class="mx-16 text-4xl">as;lkjads;lkjasdlkj</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from "vue";
+</script>
