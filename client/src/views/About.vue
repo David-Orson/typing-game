a0fc57aa1f0f53f6450bafaee5142235d87d8eb6
OrsonDC <onlyorson@outlook.com>
2022-11-20 16:43:17 +0100

    :heavy_plus_sign: Add About Page
            Basic instructions for using the application.

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container mx-auto">
+    <div class="mb-4">
+      This is a basic typing test game to measure your words per minute and
+      accuracy.
+    </div>
+    <div class="mb-4">
+      Type the same character as the test string to succeed, press "Enter" to
+      restart the test.
+    </div>
+    <div class="mb-4">
+      Personal records are saved when you score 100% accuracy and you have a new
+      highest words per minute.
+    </div>
+    <div class="mb-4">
+      Personal records are saved to an account if you create one, you can also
+      view all tests that you have completed on the account page.
+    </div>
+    Created by David Orson
   </div>
 </template>
